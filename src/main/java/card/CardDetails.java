6feea7e84decda472d7a3b3e6afa9d3242a0d6c1
Tringapps-Aarif <moin.mohammed@tringapps.com>
2023-03-21 12:29:10 +0530

    commit

--- src/main/java/card/CardDetails.java
+++ src/main/java/card/CardDetails.java
@@ -1,2 +1,29 @@
-package card;public class CardDetails {
+package card;
+
+import java.util.logging.Logger;
+
+public class CardDetails implements Cloneable {
+   private final String ogName;
+    private final int ogCardNo;
+   private final String ogExpDate;
+   final Logger print = Logger.getLogger("card");
+   public CardDetails(String ogName, int ogCardNo, String ogExpDate) {
+        this.ogName = ogName;
+        this.ogCardNo = ogCardNo;
+        this.ogExpDate = ogExpDate;
+    }
+
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+  public  boolean equals(int cardNo) {
+
+      return ogCardNo == cardNo;
+    }
+
+  public  void display() {
+      print.info(
+                "Card holder name : " + ogName + "\nCard number : " + ogCardNo + "\nCard Expiry date : " + ogExpDate);
+    }
 }
--- src/main/java/maincard/Main.java
+++ src/main/java/maincard/Main.java
@@ -1,7 +1,44 @@
-package org.example;
+package maincard;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Scanner;
+import java.util.logging.Logger;
+import card.*;
+
+class CreditCard {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        final  Scanner sc = new Scanner(System.in);
+        final Scanner sc1 = new Scanner(System.in);
+        final  Scanner sc3 = new Scanner(System.in);
+        final  Logger print = Logger.getLogger("card");
+        print.info("Enter Original card details this details will be cloned ");
+        print.info("Enter Card Holder Name : ");
+        String ogName = sc.nextLine();
+        print.info("Enter Card Number : ");
+        int ogCardNo = sc.nextInt();
+        print.info("Enter Expiry Date :");
+        String ogExpDate = sc1.nextLine();
+        CardDetails obj1 = new CardDetails(ogName, ogCardNo, ogExpDate);
+        CardDetails obj2 = (CardDetails) obj1.clone();
+        print.info("Enter Card Details to check: \n");
+        print.info("Enter Card Holder Name : ");
+        String name = sc1.nextLine();
+        print.info("Enter Card Number : ");
+        int cardNo = sc1.nextInt();
+        print.info("Enter Expiry Date : ");
+        String expDate = sc3.nextLine();
+        CardDetails obj3 = new CardDetails(name, cardNo, expDate);
+        print.info("\nThe original card details\n");
+        obj1.display();
+        print.info("\nCloned card details\n");
+        obj2.display();
+        print.info("\nThe card details we checked\n");
+        obj3.display();
+        boolean ans = obj2.equals(cardNo);
+        if (ans) {
+            print.info("\nBoth card details are same\n");
+        } else {
+            print.info("\nBoth card details are not same\n");
+        }
+       
     }
 }
